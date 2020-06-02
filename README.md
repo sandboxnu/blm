dc626dbd44c014887ca81ae45062e04f5e1e8590
dajinchu <dajinchu@gmail.com>
2020-06-02 17:18:12 -0400

    postcss

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Built to track donations for coalition of Northeastern clubs running a fundraiser for racial justice. In memory of George Floyd.
+
+Hosted by [Vercel](https://vercel.com).
--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,18 @@
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    process.env.NODE_ENV === "production"
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
       ? [
-          require("@fullhuman/postcss-purgecss"),
-          {
+          require("@fullhuman/postcss-purgecss")({
             content: [
               "./pages/**/*.{js,jsx,ts,tsx}",
               "./components/**/*.{js,jsx,ts,tsx}",
             ],
             defaultExtractor: (content) =>
-              content.match(/[w-/:]+(?<!:)/g) || [],
-          },
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }),
         ]
-      : undefined,
-    require("autoprefixer"),
+      : []),
   ],
 };
