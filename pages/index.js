289bf47a6d4c09ee6d9ced117279a31ba1ce01f9
dajinchu <dajinchu@gmail.com>
2020-06-02 19:49:15 -0400

    unmatched

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,13 @@ function Progress({ raised, total }) {
   const percent = Math.floor((raised / total) * 100);
   return (
     <div className="w-full max-w-xl">
-      <span>
+      <div className="flex items-baseline">
         <span className="text-2xl">${raised}</span>
-        <span className="text-gray-700"> matched of ${total}</span>
-      </span>
+        <span className="ml-1 text-gray-700"> matched of ${total}</span>
+        <span className="ml-auto text-gray-700">
+          ${total - raised} still unmatched!
+        </span>
+      </div>
       <div className="h-3 bg-white rounded-full shadow-xs">
         <div
           className="h-full bg-blue-800 rounded-full"
