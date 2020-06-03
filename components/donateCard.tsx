616d306382d492edf382c85ca32f7da60822a574
Arun Jeevanantham <itsmearun98@gmail.com>
2020-06-03 18:13:07 -0400

    basic donate card

--- components/donateCard.tsx
+++ components/donateCard.tsx
@@ -6,5 +6,20 @@ interface DonateCardProps {
   logo: string;
 }
 export default function DonateCard({ img, name, link, logo }: DonateCardProps) {
-  return <img src={img} />;
+  return (
+  <div className="flex justify-between w-full bg-gray-900">
+    <div className="flex">
+      <img src={img} />
+      <div className="p-4 pl-8"> 
+        <p className="text-xl font-bold pb-4"> {name} </p>
+        <a 
+          className="bg-cta hover:bg-cta text-black font-bold py-2 px-4 rounded"
+          href={link}>
+          Donate
+        </a>
+      </div>
+    </div>
+      <img src={logo} className="object-none p-4"/>
+  </div>
+);
 }
