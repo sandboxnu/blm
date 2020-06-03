65b76ef74157675717001db0584f5b42cdb75dd7
dajinchu <dajinchu@gmail.com>
2020-06-03 18:07:27 -0400

    progress cards

--- components/progress.tsx
+++ components/progress.tsx
@@ -3,15 +3,19 @@ import { FundraiserProgress } from "../lib/types";
 export default function Progress({ raised, total }: FundraiserProgress) {
   const percent = Math.floor((raised / total) * 100);
   return (
-    <div className="w-full max-w-xl">
+    <div className="w-full">
       <div className="flex items-baseline">
-        <span className="text-2xl">${raised}</span>
-        <span className="ml-1 text-gray-700"> matched of ${total}</span>
-        <span className="ml-auto text-gray-700">
-          ${total - raised} still unmatched!
+        <span className="text-3xl font-bold">${raised}</span>
+        <span className="ml-1 text-gray-400">
+          matched of
+          <span className="font-bold"> ${total}</span>
+        </span>
+        <span className="ml-auto text-gray-400">
+          <span className="mr-1 font-bold"> ${total - raised}</span>
+          still unmatched!
         </span>
       </div>
-      <div className="h-3 bg-white rounded-full shadow-xs">
+      <div className="mt-2 h-4 bg-gray-800 rounded-full shadow-xs">
         <div
           className="h-full bg-cta rounded-full"
           style={{ width: `${percent}%` }}
