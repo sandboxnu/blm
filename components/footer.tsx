4699d6e6eafb59eb3a20453f648265659c9d85fb
Ryan Drew <ryan.drew14@gmail.com>
2020-06-03 22:29:51 -0400

    Add second paragraph to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,16 @@
 export default function Footer() {
   return (
     <div className="pb-10 pt-12">
-      <div className="font-semibold">* Disclaimer</div>
-      <div className="font-light text-sm">
+      <div className="font-semibold pb-2">Disclaimer</div>
+      <div className="font-light text-sm pb-3">
         This website was created by Northeastern students intending to support
         the Black community and the Black Lives Matter movement. It is not
         affiliated with Northeastern University or with an official Black Lives
         Matter chapter.
+      </div>
+      <div className="font-light text-sm">
+        All assets in the matching fund are by individual donation only. No
+        university or student organization funds were used for this fund.
       </div>
     </div>
   );
