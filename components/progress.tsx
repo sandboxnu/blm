e78079b8d866b56691022ea156c7a7791411ff1a
Ryan Drew <ryan.drew14@gmail.com>
2020-06-03 22:52:55 -0400

    Update progress bar padding

--- components/progress.tsx
+++ components/progress.tsx
@@ -3,29 +3,31 @@ import { FundraiserProgress } from "../lib/types";
 export default function Progress({ raised, total }: FundraiserProgress) {
   const percent = Math.floor((raised / total) * 100);
   return (
-    <div className="w-full">
-      <div className="flex items-baseline">
-        <span className="text-3xl font-bold">${raised}</span>
-        <span className="ml-1 text-gray-400">
-          matched of
-          <span className="font-bold"> ${total}</span>
-        </span>
+    <div className="mt-8 mb-5">
+      <div className="w-full">
+        <div className="flex items-baseline">
+          <span className="text-4xl font-bold pr-2">${raised}</span>
+          <span className="ml-1 text-gray-400">
+            matched of
+            <span className="font-bold"> ${total}</span>
+          </span>
 
-        <span className="invisible sm:visible ml-auto text-gray-400">
+          <span className="invisible sm:visible ml-auto text-gray-400">
+            <span className="mr-1 font-bold"> ${total - raised}</span>
+            still unmatched!
+          </span>
+        </div>
+        <div className="mt-2 h-4 bg-gray-800 rounded-full shadow-xs mb-3">
+          <div
+            className="h-full bg-cta rounded-full"
+            style={{ width: `${percent}%` }}
+          ></div>
+        </div>
+        <span className="visible sm:invisible text-gray-400">
           <span className="mr-1 font-bold"> ${total - raised}</span>
           still unmatched!
         </span>
       </div>
-      <div className="mt-2 h-4 bg-gray-800 rounded-full shadow-xs mb-3">
-        <div
-          className="h-full bg-cta rounded-full"
-          style={{ width: `${percent}%` }}
-        ></div>
-      </div>
-      <span className="visible sm:invisible text-gray-400">
-          <span className="mr-1 font-bold"> ${total - raised}</span>
-          still unmatched!
-      </span>
     </div>
   );
 }
