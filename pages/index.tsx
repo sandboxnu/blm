07ea89301199e1af43c30015fbf50720d3d65d52
dajinchu <dajinchu@gmail.com>
2020-06-04 11:58:34 -0400

    some resources

--- pages/index.tsx
+++ pages/index.tsx
@@ -77,19 +77,28 @@ export default function Home({ progress }: HomeProps) {
       {donateCardArray}
       <Header
         title="2. Get Educated"
-        subtitle="It's on the people who can't directly relate to educate themselves. Here are a few of the
-          many resources that exist for education."
+        subtitle="Learn more about instituionalized racism and how you can make an impact."
       />
       <div className="grid gap-10">
         <Card
-          body="Resources from the Obama Foundation"
+          body="Collection of readings to learn to be an effective ally."
+          title="Justice in June"
+          href="https://docs.google.com/document/d/1H-Vxs6jEUByXylMS2BjGH1kQ7mEuZnHpPSs1Bpaqmw0/preview?pru=AAABcppkS48%2AWmo91FkcRICr88LdxGN2Qg&fbclid=IwAR1lA2EGbMmY6oDsq5uJrhWTC3lzpJufTd-naUKCJ0uC2iCWx4WqMRzO-hQ"
+        />
+        <Card
+          body="Report on racial disparities in arrests and use of force."
+          title="Science of Justice"
+          href="https://policingequity.org/images/pdfs-doc/CPE_SoJ_Race-Arrests-UoF_2016-07-08-1130.pdf"
+        />
+        <Card
+          body="Resources from the Obama Foundation."
           title="Anguish and action"
           href="https://www.obama.org/anguish-and-action/"
         />
         <Card
-          body="Resources from the Obama Foundation"
-          title="Anguish and action"
-          href="https://www.obama.org/anguish-and-action/"
+          body="Donate with or without money."
+          title="Other ways to help"
+          href="https://blacklivesmatters.carrd.co/#donate"
         />
       </div>
       <Header
